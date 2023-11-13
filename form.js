717d37ee00058e7977a7c561cda3fb1c006bfd0d
Vaishnavi <vaishnavi@gmail.com>
2023-11-13 17:17:29 +0000

    loginform

--- form.js
+++ form.js
@@ -4,11 +4,16 @@ const uName = document.getElementById("name");
 const email = document.getElementById("email");
 const password = document.getElementById("password");
 const successMsg = document.getElementById("successMsg");
+const registerform = document.getElementById("registerform");
+const loginform = document.getElementById("loginform");
+const login = document.getElementById("login");
 
-const loginForm = document.getElementById("loginform");
+const loginemail=document.getElementById("loginemail");
+const loginpassword=document.getElementById("loginpassword");
+const btnlogin = document.getElementById("btnlogin");
+
 
 saveDetailsBtn.addEventListener("click",async()=>{
-    // alert("vaishnavi");
     const userDetails = {
         userName : uName.value,
         userEmail : email.value,
@@ -29,9 +34,40 @@ saveDetailsBtn.addEventListener("click",async()=>{
     })
     .then((data) => {
       console.log('ds',data)
-      successMsg.textContent = "vaishu";
-      successMsg.style.color = "red";
+      successMsg.textContent = data.text;
       })
     
 })
 
+login.addEventListener("click",async()=>{
+    // alert("dyxyh")
+    loginform.style.display="block";
+    registerform.style.display="none";
+    successMsg.textContent=""
+})
+
+
+btnlogin.addEventListener("click",async()=>{
+    const userDetails = {
+        userEmail : loginemail.value,
+        userPassword: loginpassword.value
+    }
+    
+    fetch("http://localhost:8384/login",{
+        method: 'POST' ,
+        headers:{
+            'Content-Type':"application/json"
+        },
+        body:JSON.stringify(userDetails)
+
+    })
+    .then(function(response){
+        console.log('hkhjkj',response)
+        return response.json()
+    })
+    .then((data) => {
+      console.log('ds',data)
+      successMsg.textContent = data.text;
+      })
+    
+})
--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const bcrypt = require("bcrypt");
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.set("view engine","html")
+
 
 const dbPath = path.join(__dirname, "userDetails.db");
 let db = null;
@@ -37,31 +37,63 @@ app.post("/details/", async (req, res) => {
   const checkUserExistsQuery = `SELECT * FROM userDetails WHERE name = '${userName}'`;
   const getUser = await db.get(checkUserExistsQuery);
   console.log("user details fetched")
-  if (getUser === undefined) {
+  if (getUser === undefined && userName!==""  && userEmail!=="" && userPassword!=="") {
     
     if (userPassword.length < 5) {
       console.log("Password is too short");
       
-      res.send({userEmail});
+      res.send({text:"Password is too short"});
     } else {
       const hashedPassword = await bcrypt.hash(req.body.userPassword, 10);
       const createUserQuery = `INSERT INTO userDetails (name, email, password) VALUES ('${userName}', '${userEmail}', '${hashedPassword}');`;
       const createUser = await db.run(createUserQuery);
       console.log("User created successfully");
       
-      res.json({userEmail});
+      res.json({text:"User created successfully"});
     }
-  } else {
+  } 
+  else if(userName==="" || userEmail==="" || userPassword===""){
+    res.json({text:"Please fill all details"});
+  }
+  else {
     console.log("user already exists");
     
-    res.json({userEmail});
+    res.json({text:"user already exists"});
     
   }
 });
 
-app.get("/profile",(req,res)=>{
-  res.sendFile(path.join(__dirname,"profile.html"))
-})
 
 
+app.post("/login", async (req, res) => {
+  const { userEmail,userPassword } = req.body;
+  console.log(userEmail);
 
+  const loginUserQuery = `SELECT * FROM userDetails WHERE email = '${userEmail}';`;
+  const loginUser = await db.get(loginUserQuery);
+  if (loginUser !== undefined && userPassword!=="" && userEmail!=="") {
+    const isPasswordMatched = await bcrypt.compare(
+      req.body.userPassword,
+      loginUser.password
+    );
+    if (isPasswordMatched) {
+      
+      console.log("Login success!");
+      res.json({text:"Login success!"});
+    } else {
+      
+      console.log("Invalid password");
+      res.json({text:"Invalid password"});
+    }
+  } 
+  else if (userPassword==="" || userEmail===""){
+    console.log("Please fill all details");
+    res.json({text:"Please fill all details"});
+  }
+  
+  else {
+   
+    console.log("Invalid user");
+    res.json({text:"Invalid user"});
+  }
+});
